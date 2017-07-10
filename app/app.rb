e9cac0591e7ecf9337872d4f399978a74cc3faef
Jasper King-Harman <j.kingharman@googlemail.com>
2017-07-10 14:18:00 +0100

    Updating models and config db

--- app/app.rb
+++ app/app.rb
@@ -15,5 +15,6 @@ class MakersBnb < Sinatra::Base
   get '/' do
     erb :index
   end
-  
+
+
 end
--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,6 +5,8 @@ require 'dm-postgres-adapter'
 require_relative 'models/user'
 require_relative 'models/space'
 
+# need to revert back to env.
+
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/makersbnb_test#")
 DataMapper.finalize
 DataMapper.auto_upgrade!
