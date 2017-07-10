fde17f7e428c79bb9ebc6522d7aa80cdf8a002ba
tbscanlon <thomas.scanlon@outlook.com>
2017-07-10 15:05:54 +0100

    Register and validate new user accounts

--- spec/backend/features/signing_up_spec.rb
+++ spec/backend/features/signing_up_spec.rb
@@ -1,10 +1,47 @@
 feature 'User sign up' do
-  scenario "Hello world" do
-    visit '/'
-    expect(page).to have_content "hello world"
-  end
-  
   scenario "user can signup" do
     expect { sign_up }.to change { User.count }.by(1)
+  end
+
+  scenario "Sends user to homepage after registration" do
+    sign_up
+    expect(page.current_path).to eq '/'
+  end
+
+  feature 'Incorrect Sign ups' do
+    scenario 'does not make account if password confirmation is wrong' do
+      expect { sign_up(password: 'wrong') }.to_not change(User, :count)
+    end
+
+    scenario 'shows an error on non-matching passwords' do
+      sign_up(password: 'very wrong')
+      expect(page).to have_content 'Password does not match the confirmation'
+    end
+
+    feature 'Bad Emails' do
+      scenario "can't sign up without entering an email address" do
+        expect { sign_up(email: nil) }.to_not change(User, :count)
+        expect(page).to have_content "Email must not be blank"
+      end
+
+      scenario "can't sign up twice with the same email" do
+        sign_up
+        expect { sign_up }.to_not change(User, :count)
+        expect(page).to have_content "Email is already taken"
+      end
+    end
+
+    feature "Bad Usernames" do
+      scenario "can't sign up without a username" do
+        expect { sign_up(username: nil) }.to_not change(User, :count)
+        expect(page).to have_content "Username must not be blank"
+      end
+
+      scenario "can't sign up twice with the same username" do
+        sign_up
+        expect { sign_up }.to_not change(User, :count)
+        expect(page).to have_content "Username is already taken"
+      end
+    end
   end
 end
