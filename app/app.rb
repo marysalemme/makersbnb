4b3cde387705ec60a2114888a090a7db4e0018a4
Verity <vhonebon@gmail.com>
2017-07-11 10:50:09 +0100

    changes made to space routes

--- app/app.rb
+++ app/app.rb
@@ -18,18 +18,23 @@ class MakersBnb < Sinatra::Base
     erb :index
   end
 
-  get '/space' do
+  get '/space/new' do
     erb :'space/new'
   end
 
   post '/space' do
     @space = Space.new(description: params[:description], price: params[:price], location: params[:location])
     if @space.save
-      erb :'space/index'
+      redirect 'space/index'
     else
       flash.now[:errors] = @space.errors.full_messages
       erb :'space/new'
     end
+  end
+
+  get '/space/index' do
+    @space = Space.last
+    erb :'space/index'
   end
 
   get '/users/new' do
--- spec/backend/features/create_space_spec.rb
+++ spec/backend/features/create_space_spec.rb
@@ -1,7 +1,7 @@
 
 feature "list of spaces" do
   scenario "user creates a space" do
-    visit '/space'
+    visit '/space/new'
     fill_in "description", with: "tiny flat"
     fill_in "price", with: "50.0"
     fill_in "location", with: "Mile End"
@@ -14,7 +14,7 @@ end
 
 feature "correct errors are thrown" do
   scenario "blank fields throw an error" do
-    visit '/space'
+    visit '/space/new'
     fill_in "description", with: "tiny flat"
     fill_in "price", with: "50.0"
     click_on("List space")
